cc37887bf45521b9485e56efe285fce6abd3a838
yeab-B <yeabsirabehailu92@gmail.com>
2025-07-16 18:44:57 +0300

    init

--- auth/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\AuthController;
-
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-Route::get('/user', [AuthController::class, 'index'])->middleware('auth:sanctum');
